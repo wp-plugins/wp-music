f2f4646a092f00fe238a0119e665c6cfad27b5e3
righTune <righTune@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-04 11:13:06 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/wp-music/trunk@782523 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+﻿=== WordPress Music ===
+Contributors: righTune 
+Tags: music,background music,music player,righTune,engagement,ecommerce,audio,mp3,content
+Requires at least: 2.7
+Tested up to: 3.7
+Stable tag: 1.0
+
+Engage your users emotionally by playing relevant, customized music. WordPress Music is the easiest way to play background music on your site. FREE.
+
+== Description ==
+
+Our music creates a subtle, ubiquitous ambiance that adds a deep emotional dimension to a site\'s content and directly impacts user behavior and increases staying time, loyalty and revenue. We even take it one step further; while playing music, we track users behavior and engagement level on your website. We use behavioral, machine learning algorithm that continually updates the music in order to increse users\' engagement. We use our own huge catalog to choose fresh, relevant playlists on an ongoing basis.  
+Key features:  
+♪ Daily fresh music from our own huge catalog. No more repetitive or overused music.  
+♪ You set the mood, we\'ll set the background music for you. We learn what playlists perform best for you.  
+♪ Allows full customization of the player.  
+♪ Fully integrated with WordPress platform. Doesn\'t delay website\'s page load time.  
+♪ Easy to install. In less than 30 seconds you are up and running.  
+♪ Speedy response for any support request.
+
+== Installation ==
+
+1. Upload the plugin to the \`/wp-content/plugins/\` directory
+2. Activate the plugin through the \'Plugins\' menu in WordPress  
+3. Follow the instructions on the plugin settings page</p>
+== Frequently Asked Questions ==
+
+You can find our detailed FAQ page [here]("http://www.rightune.com/faq).  
+== Screenshots ==
+1. righTune background music player
+
+== Changelog ==
+= 1.0 =
+ 
+* Initial version
---
+++ rightune.php
@@ -0,0 +1,77 @@
+<?php
+/*
+  Plugin Name: WordPress Music plugin by righTune
+  Plugin URI: http://www.rightune.com
+  Description: WordPress Music is the easiest way to play background music on your WordPress website. Engage your users emotionally by playing relevant, customized background music on your websites. 
+  Version: 1.0
+  Author: righTune
+  Author URI: http://www.rightune.com
+ */
+
+// add the admin options page
+add_action('admin_menu', 'rightune_admin_add_page');
+
+function rightune_admin_add_page() {
+    add_options_page('righTune Settings', 'WordPress Music', 'manage_options', 'rightune', 'rightune_options_page');
+}
+
+// display the admin options page
+function rightune_options_page() {
+    ?>
+
+    <div>
+        <h2><?php _e('WordPress Music plugin by righTune','righttune'); ?></h2>
+        <p>
+	WordPress Music is the easiest way to play background music on your WordPress website. Engage your users emotionally by playing relevant, customized background music on your websites. Sign up for FREE at 		<a href="http://dashboard.rightune.com/signup?tag=wp" target="_blank">this page</a>.	
+	</p>
+	<p>
+	<strong>Follow this steps</strong> to generate your own unique player:
+	<ol>
+		<li>Go to righTune's <a href="http://dashboard.rightune.com/signup?tag=wp" target="_blank">sign up page</a> to customize your player.</li>	
+		<li> Copy the code you'll get at the last step.</li>
+		<li>Paste code at this box and Click the Save Changes button below.</li>
+	</ol>
+	</p>
+        <form action="options.php" method="post">
+            <?php settings_fields('rightune_options'); ?>
+            <?php do_settings_sections('rightune'); ?>
+
+            <input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+        </form></div>
+
+    <?php
+}
+
+// add the admin settings and such
+add_action('admin_init', 'rightune_admin_init');
+
+function rightune_admin_init() {
+    register_setting('rightune_options', 'rightune_options', 'rightune_options_validate');
+    add_settings_section('plugin_main', 'Script Settings', 'rightune_section_text', 'rightune');
+    add_settings_field('plugin_script_code', 'Paste script code here', 'rightune_setting_string', 'rightune', 'plugin_main');
+}
+
+function rightune_section_text() {
+    _e('','rightune');
+}
+
+function rightune_setting_string() {
+    $options = get_option('rightune_options');
+    echo "<textarea rows='4' cols='50' id='plugin_script_code' name='rightune_options[script_code]'>{$options['script_code']}</textarea>";
+}
+
+// validate our options (doing nothing here...)
+function rightune_options_validate($input) {
+    $options = get_option('rightune_options');
+    $options['script_code'] = trim($input['script_code']);
+    return $options;
+}
+
+// ACtually doing something
+
+add_action('wp_head', 'rightune_add_script');
+
+function rightune_add_script() {
+    $options = get_option('rightune_options');
+    echo $options['script_code'];
+}
